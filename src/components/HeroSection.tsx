1f279a21faea5d58a395c4527fe84474c4b41ca1
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-12-08 01:24:13 +0530

    made the herosection tailwind classes better optimised

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,15 +13,13 @@ export default function HeroSection() {
         className="w-full h-72 md:h-[38rem] bg-cover bg-bottom bg-no-repeat"
         style={{ backgroundImage: `url(${HeroImage.src})` }}
       >
-        <div className="w-full h-72 md:h-[38rem] flex flex-col justify-end items-start pb-6 md:pb-14 backdrop-brightness-50">
-          <h3
-            className={`text-slate-200 text-3xl md:text-7xl pl-4 md:pl-10 ${caveat.className} font-bold antialiased`}
-          >
+        <div
+          className={`w-full h-72 md:h-[38rem] flex flex-col justify-end items-start pb-6 md:pb-14 backdrop-brightness-50 ${caveat.className} font-semibold antialiased`}
+        >
+          <h3 className="text-slate-200 text-3xl md:text-7xl pl-4 md:pl-10">
             Elevate&nbsp;Your&nbsp;Stay&nbsp;to
           </h3>
-          <h3
-            className={`text-slate-200 text-3xl md:text-7xl pl-4 md:pl-10 ${caveat.className} font-bold antialiased`}
-          >
+          <h3 className="text-slate-200 text-3xl md:text-7xl pl-4 md:pl-10">
             Serenity&nbsp;and&nbsp;Sophistication
           </h3>
         </div>
