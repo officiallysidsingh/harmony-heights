ee8e32809e33d29bf56e2b22a6f42a9c14b110cb
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-12-11 00:13:49 +0530

    changed navbar to support all the required pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import About from "@/components/About";
+import RoomDesc from "@/components/RoomDesc";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      <Navbar buttonType={"Login"} />
       <HeroSection />
       <About />
+      <RoomDesc />
     </div>
   );
 }
+// <Footer />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,17 @@ import { useState, useEffect } from "react";
 
 // Font Import
 import { Ephesis } from "next/font/google";
+import Link from "next/link";
+
+// Next Auth Import
+import { signOut } from "next-auth/react";
 
 const ephesis = Ephesis({
   subsets: ["latin"],
   weight: "400",
 });
 
-export default function Navbar() {
+export default function Navbar({ buttonType }: { buttonType: string }) {
   const [show, setShow] = useState("top");
   const [lastScrollY, setLastScrollY] = useState(0);
 
@@ -27,6 +31,10 @@ export default function Navbar() {
     }
   };
 
+  const logoutRedirect = () => {
+    signOut();
+  };
+
   useEffect(() => {
     window.addEventListener("scroll", controlNavbar);
 
@@ -37,12 +45,16 @@ export default function Navbar() {
 
   return (
     <div
-      className={`fixed z-50 w-full text-slate-100 transition-all ease-in-out duration-500 ${show === "top"
-          ? ""
-          : show === "show"
-            ? "backdrop-filter backdrop-blur-sm bg-opacity-50 bg-black"
-            : "-translate-y-20"
-        }`}
+      className={`z-50 w-full text-slate-100 transition-all ease-in-out duration-500 ${buttonType === "Login"
+          ? show === "top"
+            ? "fixed"
+            : show === "show"
+              ? "fixed backdrop-filter backdrop-blur-sm bg-opacity-50 bg-black"
+              : "fixed -translate-y-20"
+          : show === "top"
+            ? "sticky backdrop-filter backdrop-blur-lg bg-opacity-90 bg-black"
+            : ""
+        } `}
     >
       <div className="flex justify-between items-center pt-2 pb-1 px-6 md:px-12">
         <div className={`flex flex-col ${ephesis.className} antialiased`}>
@@ -52,9 +64,26 @@ export default function Navbar() {
           <h2 className="text-xl md:text-2xl font-bold">Heights</h2>
         </div>
         <div className="flex justify-between items-center gap-5 font-semibold">
-          <div className="border-2 border-action px-5 py-1.5 hover:bg-action hover:text-black rounded-md cursor-pointer">
-            <h3 className="font-semibold">Login</h3>
-          </div>
+          {buttonType === "Log Out" ? (
+            <div
+              onClick={logoutRedirect}
+              className="border-2 border-action px-5 py-1.5 hover:bg-action hover:text-black rounded-md cursor-pointer"
+            >
+              <h3 className="font-semibold">{buttonType}</h3>
+            </div>
+          ) : (
+            <Link
+              href={{
+                pathname: "/login",
+                query: {
+                  fromBooking: false,
+                },
+              }}
+              className="border-2 border-action px-5 py-1.5 hover:bg-action hover:text-black rounded-md cursor-pointer"
+            >
+              <h3 className="font-semibold">{buttonType}</h3>
+            </Link>
+          )}
         </div>
       </div>
     </div>
