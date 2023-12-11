102370fa3e70284b122989ca3f452169c5634f05
officiallysidsingh <siddharthsingh2014@gmail.com>
2023-12-11 19:35:07 +0530

    deleted seperate payment route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/redirect", "/dashboard", "/profile", "/payment"],
+  matcher: ["/redirect", "/dashboard", "/profile", "/booking"],
 };
